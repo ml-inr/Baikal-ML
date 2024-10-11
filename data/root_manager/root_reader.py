550ed007423c7d6744e57fc99656be2517effc7e
Albert Mateiko <albertmatseiko@gmail.com>
2024-10-11 22:12:00 +0300

    pandas --> polars. Improved performance

--- data/root_manager/root_reader.py
+++ data/root_manager/root_reader.py
@@ -1,12 +1,15 @@
 from typing import Optional
 
 import numpy as np
-import pandas as pd
+
+# import pandas as pd
+import polars as pl
 import uproot as ur
 import awkward as ak
 from functools import lru_cache
 
 from data.root_manager.root_paths import RootPaths, BaseFeaturePaths
+from data.root_manager.polars_schema import DataSchema
 from data.root_manager.constants import Constants as Cnst
 
 
@@ -15,176 +18,164 @@ class BadFile(Exception):
 
 
 class RootReader:
-    
+
     def _check_file(self) -> bool:
         try:
-            ev_num = self.PulsesN.num_entries
-            return ev_num > 1 or (ev_num == 1 and self.PulsesN.array(library="np", entry_start=0, entry_stop=1)[0] != 0)
+            return self.ev_num > 1 or (
+                self.ev_num == 1
+                and self.rf[self.paths.ev_paths.PulsesN].array(library="np")[0] != 0
+            )
         except Exception:
             return False
-    
+
     def get_start_index(self):
-        test = self.rf['Events/BEvent./BEvent.fPulseN'].array(library="np", entry_start=0, entry_stop=1)[0]
+        test = self.rf["Events/BEvent./BEvent.fPulseN"].array(
+            library="np", entry_start=0, entry_stop=1
+        )[0]
         return 1 if test == 0 else 0
-    
-    def __init__(self, root_file: ur.reading.ReadOnlyDirectory, 
-                 paths: RootPaths = RootPaths(), prefix: str = ''):
+
+    def __init__(
+        self,
+        root_file: ur.reading.ReadOnlyDirectory,
+        paths: RootPaths = RootPaths(),
+        prefix: str = "",
+    ):
         self.rf = root_file
         self.paths = paths
+        self.ev_paths = self.paths.ev_paths.to_dict()
+        self.pulses_paths = self.paths.pulses_paths.to_dict()
+        self.ind_mu_paths = self.paths.ind_mu_paths.to_dict()
+        self.all_data_paths = {
+            **self.paths.ev_paths.to_dict(),
+            **self.paths.pulses_paths.to_dict(),
+            **self.paths.ind_mu_paths.to_dict(),
+        }
+
         self.prefix = prefix
-        self.PulsesN = self.rf[self.paths.ev_paths.PulsesN]
+        self.ev_num = self.rf[self.paths.ev_paths.PulsesN].num_entries
         if not self._check_file():
             raise BadFile("no valid events in file")
-        self.start = self.get_start_index()  
-        self.events = None
-        self.data = None
-    
-    # def _get_dict_from_paths(self, paths: BaseFeaturePaths) -> dict:
-    #     """
-    #     Reads root data as an arrays of objects
-    #     and converts to Python dict.
-    #     Returns: dict[str][object]
-    #     """
-    #     result_dict = {}
-    #     for name, path in paths.to_dict().items():
-    #         value = self.rf[path].array(library="np")[self.start:].astype(object)
-    #         result_dict[name] = value
-    #     return result_dict
-    
-    @lru_cache(maxsize=None)
-    def _get_array(self, path):
-        return self.rf[path].array(library="np", entry_start=self.start)
-    # def _get_array(self, path):
-    #     return self.rf[path].array(library="np")[self.start:].astype(object)
-    
-    def _get_dict_from_paths(self, paths: BaseFeaturePaths) -> dict:
-        """
-        Reads root data as an arrays of objects
-        and converts to Python dict.
-        Returns: dict[str][object]
-        """
-        result_dict = {}
-        for name, path in paths.to_dict().items():
-            value = self._get_array(path)
-            result_dict[name] = value
-        return result_dict
-    
-    def _add_prefix_to_id(self, ids: pd.Series):
-        ids = self.prefix + ids.astype(str)
-        return ids
-    
-    def _flatten_df(self, df: pd.DataFrame) -> pd.DataFrame:
-        N = df.iloc[:, 0].apply(len).to_numpy()
-        ev_ids = np.repeat(np.arange(len(df)), N)
-        element_ids = np.concatenate([np.arange(n) for n in N])
-        
-        flat_dict = {feature: np.concatenate(df[feature].to_list()) for feature in df.columns.to_list()}
-        flat_dict['ev_id'] = ev_ids
-        flat_dict['element_id'] = element_ids
-        
-        return pd.DataFrame(flat_dict)
+        self.ev_ids = pl.Series(
+            name="ev_id", values=[f"{self.prefix}{i}" for i in range(self.ev_num)]
+        )
+        self.start = self.get_start_index()
+        self.df_schema = DataSchema().to_dict()
 
-    # def _flatten_df(self,df: pd.DataFrame) -> pd.DataFrame:
-    #     df['N'] = df.iloc[:, 0].apply(len)
-    #     df['ev_id'] = df.reset_index()['index'].apply(lambda x: [x]*df['N'].iloc[x])
-    #     df['element_id'] = df.reset_index()['index'].apply(lambda x: [*range(df['N'].iloc[x])])
-    #     df = df.drop(columns=['N'])
-    #     flat_dict = {feature: np.concatenate(df[feature].to_list()) for feature in df.columns.to_list()}
-    #     return pd.DataFrame(flat_dict)
-        
-    def read_events_as_df(self) -> pd.DataFrame:
-        """
-        Reads aggregated features about events as a whole
-        and converts to pd.Dataframe.
-        
-        Returns:
-            pd.Datarame[
-                PulsesN[int],
-                PrimeTheta[float], PrimePhi[float], PrimeEn[float], PrimeNuclN[int],
-                ResponseMuN[int], BundleEnReg[float], 
-                EventWeight[float],
-                RespMuTheta[np.array[float]], RespMuPhi[np.array[float]],
-                RespMuTrackX[np.array[float]], RespMuTrackY[np.array[float]], RespMuTrackZ[np.array[float]],
-                RespMuDelay[np.array[float]], RespMuEn[np.array[float]],
-                ev_id[int]
-                ]
-        """
-        result_dict = self._get_dict_from_paths(self.paths.ev_paths)
-        df_ev = pd.DataFrame(result_dict)
-        df_ev['ev_id'] = df_ev.index
-        df_ev['ev_id'] = self._add_prefix_to_id(df_ev['ev_id'])
-        for column in ['PulsesN','PrimeNuclN','ResponseMuN']:
-            df_ev[column] = df_ev[column].astype(np.int32)
-        return df_ev
-    
-    def read_pulses_as_df(self) -> pd.DataFrame:
-        """
-        Reads pulses features from root as array per event ->
-        Unnests arrays and converts to one pd.Dataframe ->
-        orders frame by event_id and PulsesTime.
-        
-        Returns:
-            pd.Datarame[
-                PulsesChID[int], 
-                PulsesAmpl[float], 
-                PulsesTime[float], 
-                PulsesFlg[int], 
-                ev_id[int],
-                is_signal[bool]
-                ]
-        """
-        result_dict = self._get_dict_from_paths(self.paths.pulses_paths)
-        df_flat = self._flatten_df(pd.DataFrame(result_dict))
-        df_flat = df_flat.sort_values(['ev_id', 'PulsesTime'])
-        
-        df_flat['ev_id'] = self._add_prefix_to_id(df_flat['ev_id'])
-        df_flat = df_flat.drop(columns=['element_id'])
-        df_flat['PulsesChID'] = df_flat['PulsesChID'].astype(np.int16)
-        df_flat['is_signal'] = (df_flat['PulsesFlg']!=0).astype(bool)
-        df_flat['mu_local_id'] = (df_flat['PulsesFlg']%1_000_000-1).astype(np.int16)
-        df_flat['cluster_id'] = (df_flat['PulsesChID']//Cnst.CHANNEL_DIVISOR).astype(np.int8)
-        df_flat['string_id'] = (df_flat['PulsesChID']//Cnst.STRING_DIVISOR).astype(np.int8)
-        return df_flat
-    
-    def read_ind_mu_as_df(self):
-        result_dict = self._get_dict_from_paths(self.paths.ind_mu_paths)
-        df_flat = self._flatten_df(pd.DataFrame(result_dict))
-        df_flat['ev_id'] = self._add_prefix_to_id(df_flat['ev_id'])
-        df_flat = df_flat.rename(columns={'element_id': 'mu_local_id'})
-        return df_flat
+    @lru_cache(maxsize=None)
+    def _get_event_arrays(self):
+        header = "Events"
+        names = self.ev_paths.keys()
+        root_paths = [p[7:] for p in self.ev_paths.values()]
+        arrs = [
+            list(a)
+            for a in self.rf[f"{header}"]
+            .arrays(root_paths, library="np", entry_start=self.start)
+            .values()
+        ]
+        return names, arrs
+
+    @lru_cache(maxsize=None)
+    def _get_pulses_arrays(self):
+        header = "Events"
+        names = self.pulses_paths.keys()
+        root_paths = [p[7:] for p in self.pulses_paths.values()]
+        arrs = [
+            list(a)
+            for a in self.rf[f"{header}"]
+            .arrays(root_paths, library="np", entry_start=self.start)
+            .values()
+        ]
+        return names, arrs
+
+    @lru_cache(maxsize=None)
+    def _get_muons_arrays(self):
+        header = "Events"
+        names = self.ind_mu_paths.keys()
+        root_paths = [p[7:] for p in self.ind_mu_paths.values()]
+        arrs = [
+            list(a)
+            for a in self.rf[f"{header}"]
+            .arrays(root_paths, library="np", entry_start=self.start)
+            .values()
+        ]
+        return names, arrs
+
+    @lru_cache(maxsize=None)
+    def _get_all_arrays(self):
+        header = "Events"
+        names = list(self.all_data_paths.keys())
+        root_paths = [p[7:] for p in self.all_data_paths.values()]
+        arrs = [
+            list(a)
+            for a in self.rf[f"{header}"]
+            .arrays(root_paths, library="np", entry_start=self.start)
+            .values()
+        ]
+        return names, arrs
+
+    def read_events_as_df(self) -> pl.DataFrame:
+        names, arrs = self._get_event_arrays()
+        df = pl.DataFrame(arrs, schema=names)
+        df = df.cast({k: v for k, v in self.df_schema.items() if k in names})
+        df = df.with_columns(self.ev_ids)
+        return df
+
+    def read_pulses_as_df(self) -> pl.DataFrame:
+        names, arrs = self._get_pulses_arrays()
+        df = pl.DataFrame(arrs, schema=names)
+        df = df.cast({k: v for k, v in self.df_schema.items() if k in names})
+        df = df.with_columns(self.ev_ids)
+        return df
+
+    def read_muons_as_df(self) -> pl.DataFrame:
+        names, arrs = self._get_muons_arrays()
+        df = pl.DataFrame(arrs, schema=names)
+        df = df.cast({k: v for k, v in self.df_schema.items() if k in names})
+        df = df.with_columns(self.ev_ids)
+        return df
+
+    def read_all_data_as_df(self) -> pl.DataFrame:
+        names, arrs = self._get_all_arrays()
+        df = pl.DataFrame(arrs, schema=names)
+        df = df.cast({k: v for k, v in self.df_schema.items() if k in names})
+        df = df.with_columns(self.ev_ids)
+        return df
 
     def read_OM_coords(self):
-        coords_array = np.array(ak.unzip(self.rf[self.paths.geom_path].array()))[:,self.start]
-        df = pd.DataFrame(coords_array.T, columns=['X','Y','Z'], dtype=np.float64)
-        df = df.reset_index()
-        df = df.rename(columns={'index': 'PulsesChID'})
-        df['PulsesChID'] = df['PulsesChID'].astype(np.int16)
-        df['cluster_id'] = df['PulsesChID']//Cnst.CHANNEL_DIVISOR
-        df['string_id'] = df['PulsesChID']//Cnst.STRING_DIVISOR
+        coords_array = np.array(ak.unzip(self.rf[self.paths.geom_path].array()))[
+            :, 0, :
+        ]
+        df = pl.DataFrame(coords_array.T, schema=["X", "Y", "Z"])
+        df = df.with_columns(
+            pl.Series(name="PulsesChID", values=range(df.shape[0]), dtype=pl.Int16)
+        )
+        df = df.with_columns(
+            (pl.col("PulsesChID") // Cnst.CHANNEL_DIVISOR)
+            .alias("cluster_id")
+            .cast(pl.Int8)
+        )
+        df = df.with_columns(
+            (pl.col("PulsesChID") // Cnst.STRING_DIVISOR).alias("string_id")
+        )
         #   Add info about clusters centers
-        cl_centers = df[['X','Y','Z', 'cluster_id']].groupby(['cluster_id']).mean()
-        cl_centers = cl_centers.rename(columns={'X': 'Xc', 'Y': 'Yc', 'Z': 'Zc'})
-        df = df.join(cl_centers, on='cluster_id')
+        cl_centers = (
+            df[["X", "Y", "Z", "cluster_id"]]
+            .group_by(["cluster_id"])
+            .mean()
+            .rename({"X": "Xc", "Y": "Yc", "Z": "Zc"})
+        )
+        df = df.join(cl_centers, on="cluster_id")
         #   Add coords, relatively to the centers
-        for coord in ['X','Y','Z']:
-            df[f"{coord}rel"] = df[f"{coord}"]-df[f"{coord}c"]
+        expressions = []
+        for coord in ["X", "Y", "Z"]:
+            expressions.append(
+                (pl.col(coord) - pl.col(f"{coord}c")).alias(f"{coord}rel")
+            )
+        df = df.with_columns(*expressions)
         return df
 
 
-if __name__=='__main__':
-    '''
+if __name__ == "__main__":
+    """
     Usage example
-    '''
-    eventss = []
-    datas = []
-    for path in ['/home/albert/Baikal2/data/mock_MC_2020/muatm/root/all/66470.root']:
-        with open(path) as rf:
-            rr = RootReader(rf)
-            events = rr.get_events()
-            data = rr.get_data()
-        eventss.append(events), datas.append(data)
-    events = np.concatenate(eventss)
-    data = np.concatenate(datas)
-    ...
-        
-        
+    """
--- install_venv.sh
+++ install_venv.sh
@@ -9,7 +9,7 @@ activate_here venv
 # Make sure youre python, tensorflow and cuda versions are compatible!
 # The build works for CUDA Version = 11.2 (GPU GeForce GTX 1080 and 2080)
 conda install python=3.10 -y
-conda install jupyter numpy scipy matplotlib plotly pandas h5py sqlalchemy tqdm sqlite3 -y
+conda install jupyter numpy scipy matplotlib plotly pandas polars pyarrow h5py tqdm -y
 conda install -c conda-forge uproot -y
 #conda install -c conda-forge tensorflow-gpu=2.11 -y
 
